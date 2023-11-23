9427a01ec39093011aea719f74ca8f244ed58cbc
Jon <ulyger23almaty@gmail.com>
2023-11-23 16:56:37 +0800

    initial commit

---
+++ ecomm-frontend/ecomm/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+    import { goto } from '$app/navigation';
+
+    function navigateToSignUp() {
+    goto('/users/new');
+    }
+  </script>
+
+<button on:click={navigateToSignUp}>Sign Up</button>
+
+  <slot />
