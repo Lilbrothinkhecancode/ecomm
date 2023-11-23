5f5da33e3ea20a1ce32b24ed2db051ae4b568bba
Jon <ulyger23almaty@gmail.com>
2023-11-23 17:46:16 +0800

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express"
 import prisma from "./src/utils/prisma.js"
+import { Prisma, PrismaClient } from '@prisma/client'
 
 const app = express()
 const port = process.env.PORT || 8080
 
+function filter(obj, ...keys) {
+  return keys.reduce((a, c) => ({ ...a, [c]: obj[c]}), {})
+}
+
 app.use(express.json())
-app.get('/', async ( res) => {
+app.get('/', async (req, res) => {
   const allUsers = await prisma.user.findMany()
   res.json(allUsers)
 })
@@ -19,15 +24,27 @@ app.get('/user/:id', async (req, res) => {
 })
 
 app.post('/', async (req, res) => {
-    const { name, email, password } = req.body
-    const newUser = await prisma.user.create({
-      data: { name, email, password },
-    })
-  
-    console.log(newUser)
+  const { name, email, password } = req.body
+  try {
+      const newUser = await prisma.user.create({
+          data: { name, email, password },
+      })
 
-    res.json(newUser)
-  })
+      console.log(newUser)
+
+      const filteredUser = filter(newUser, 'id', 'name', 'email')
+      res.json(filteredUser)
+  } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+          const formattedError = {}
+          formattedError[`${err.meta.target[0]}`] = 'already taken'
+          return res.status(500).send({
+              error: formattedError
+          }); 
+      }
+      throw err
+  }
+})
 
   app.put('/user/:id', async (req, res) => {
     const { id } = req.params
