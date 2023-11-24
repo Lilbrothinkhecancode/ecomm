f9e091e42c1c2cee713068e0e5644c31dfaa5954
Jon <ulyger23almaty@gmail.com>
2023-11-24 15:21:55 +0800

    third commit

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 import express from "express"
 import prisma from "./src/utils/prisma.js"
 import { Prisma, PrismaClient } from '@prisma/client'
+import cors from 'cors'
+import bodyParser from 'body-parser';
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 8080
 
 function filter(obj, ...keys) {
   return keys.reduce((a, c) => ({ ...a, [c]: obj[c]}), {})
 }
 
-app.use(express.json())
+function validateUser(input) {
+  const validationErrors = {}
+
+  if (!input || !('name' in input) || !input['name'] || input['name'].length == 0) {
+    validationErrors['name'] = 'cannot be blank'
+  }
+
+  if (!input?.email || input.email.length === 0) {
+    validationErrors.email = 'cannot be blank';
+  }
+  
+  if (!input?.password || input.password.length === 0) {
+    validationErrors.password = 'cannot be blank';
+  }
+  
+  if (input?.password && input.password.length < 8) {
+    validationErrors.password = 'should be at least 8 characters';
+  }
+  
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (input?.email && !emailRegex.test(input.email)) {
+    validationErrors.email = 'is invalid';
+  }
+
+  return validationErrors
+}
+
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', async (req, res) => {
   const allUsers = await prisma.user.findMany()
   res.json(allUsers)
@@ -25,6 +56,10 @@ app.get('/user/:id', async (req, res) => {
 
 app.post('/', async (req, res) => {
   const { name, email, password } = req.body
+  const validationErrors = validateUser({ name, email, password })
+  if (Object.keys(validationErrors).length != 0) return res.status(400).send({
+    error: validationErrors
+  })
   try {
       const newUser = await prisma.user.create({
           data: { name, email, password },
