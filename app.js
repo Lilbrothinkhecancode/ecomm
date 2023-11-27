507eee84a677fa715303a20442b17d5abaf37502
Jon <ulyger23almaty@gmail.com>
2023-11-27 16:48:45 +0800

    monday commit

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 import express from "express"
+import morgan from "morgan"
 import userRouter from "./src/controllers/users.controllers.js"
 import authRouter from "./src/controllers/auth.controllers.js"
 import bodyParser from "body-parser"
 import cors from 'cors'
-const app = express()
 
+const app = express();
+app.use(morgan('combined'));
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json())
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
 app.use('/users', userRouter)
 app.use('/sign-in', authRouter)
--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,3 +1,6 @@
+import sgMail from '@sendgrid/mail'
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.development' })
 import express from 'express'
 import bcrypt from "bcryptjs"
 import { Prisma } from "@prisma/client"
@@ -6,6 +9,9 @@ import { validateUser } from "../validators/users.js"
 import { filter } from "../utils/common.js"
 import cors from 'cors'
 import bodyParser from 'body-parser';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
 const router = express.Router()
 
 const app = express();
@@ -14,32 +20,54 @@ app.use(cors());
 app.use(bodyParser.json());
 
 router.post('/', async (req, res) => {
-    const data = req.body
-  
-    const validationErrors = validateUser(data)
-  
-    if (Object.keys(validationErrors).length != 0) return res.status(400).send({
-      error: validationErrors
-    })
-  
-    data.password = bcrypt.hashSync(data.password, 8);
-  
-    prisma.user.create({
-      data
-    }).then(user => {
-      return res.json(filter(user, 'id', 'name', 'email'))
-  
-    }).catch(err => {
-      if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
-        const formattedError = {}
-        formattedError[`${err.meta.target[0]}`] = 'already taken'
-  
-        return res.status(500).send({
-          error: formattedError
-        })
-      }
-      throw err
-    })
+  const { passwordConfirm, ...data } = req.body
+
+  const validationErrors = validateUser(data)
+
+  if (Object.keys(validationErrors).length != 0) return res.status(400).send({
+    error: validationErrors
   })
+
+
+  if (data.password !== passwordConfirm) {
+
+  }
+
+  data.password = bcrypt.hashSync(data.password, 8);
+
+  prisma.user.create({
+    data
+  }).then(user => {
+    const msg = {
+      to: user.email,
+      from: 'ulyger23almaty@gmail.com',
+      subject: 'Welcome to our platform',
+      text: 'Thank you for signing up!',
+      html: '<strong>Thank you for signing up!</strong>',
+    }
+
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+
+    return res.json(filter(user, 'id', 'name', 'email'))
+
+  }).catch(err => {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const formattedError = {}
+      formattedError[`${err.meta.target[0]}`] = 'already taken'
+
+      return res.status(500).send({
+        error: formattedError
+      })
+    }
+    throw err
+  })
+})
   
   export default router
